a43a605ee35c23d0c3e369ae39e3872d67aa9758
Icaro Silva <icaro.crsilva@gmail.com>
2023-04-20 05:22:47 -0300

    Criação do MVC referente ao Usuário

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,58 @@
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Biblioteca.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+      public IActionResult Cadastro()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario u)
+        {
+            UsuarioService usuarioService = new UsuarioService();
+
+            if(u.Id == 0)
+            {
+                usuarioService.Inserir(u);
+            }
+            else
+            {
+              usuarioService.Atualizar(u);
+            }
+            
+
+            return RedirectToAction("Listagem");
+        }
+
+           public IActionResult Listagem(string tipoFiltro, string filtro)
+        {
+            
+            FiltrosUsuarios objFiltro = null;
+            if(!string.IsNullOrEmpty(filtro))
+            {
+                objFiltro = new FiltrosUsuarios();
+                objFiltro.Filtro = filtro;
+                objFiltro.TipoFiltro = tipoFiltro;
+            }
+            UsuarioService usuarioService = new UsuarioService();
+            return View(usuarioService.ListarTodos(objFiltro));
+        }
+
+         public IActionResult Edicao(int id)
+        {
+            
+            UsuarioService us = new UsuarioService();
+            Usuario u = us.ObterPorId(id);
+            return View(u);
+        }
+
+    }
+}
---
+++ Models/UsuarioService.cs
@@ -0,0 +1,73 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Biblioteca.Models
+{
+    public class UsuarioService
+    {
+        public void Inserir(Usuario u)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                bc.Usuarios.Add(u);
+                bc.SaveChanges();
+            }
+        }
+
+          public void Atualizar(Usuario u)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                Usuario usuario = bc.Usuarios.Find(u.Id);
+                usuario.Login = u.Login;
+                usuario.Senha = u.Senha;
+                                
+                bc.SaveChanges();
+            }
+        }
+
+        public ICollection<Usuario> ListarTodos(FiltrosUsuarios filtro = null)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                IQueryable<Usuario> query;
+                
+                if(filtro != null)
+                {
+                    //definindo dinamicamente a filtragem
+                    switch(filtro.TipoFiltro)
+                    {
+                        case "Login":
+                            query = bc.Usuarios.Where(u => u.Login.Contains(filtro.Filtro));
+                        break;
+
+                        case "Senha":
+                            query = bc.Usuarios.Where(u => u.Senha.Contains(filtro.Filtro));
+                        break;
+
+                        default:
+                            query = bc.Usuarios;
+                        break;
+                    }
+                }
+                else
+                {
+                    // caso filtro não tenha sido informado
+                    query = bc.Usuarios;
+                }
+                
+                //ordenação padrão
+                return query.OrderBy(u => u.Login).ToList();
+            }
+        }
+
+        public Usuario ObterPorId(int id)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                return bc.Usuarios.Find(id);
+            }
+        }
+    }
+}
